6050b84ce585c07437705ea3f7e405027e174afb
testDevops <diego.ignacio.cc@gmail.com>
2022-12-16 15:10:46 -0300

    test day1

--- testDevOps.py
+++ testDevOps.py
@@ -1,1 +1,13 @@
-print("helloworld")
+import requests
+
+# URL a la que se va a hacer la solicitud
+url = 'https://exercism.org/tracks/python'
+
+# Hacer la solicitud GET
+response = requests.get(url)
+
+# Mostrar el código de estado de la respuesta
+print(response.status_code)
+
+# Mostrar el contenido de la respuesta
+print(response.text)
